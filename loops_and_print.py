7cee1d5f660b3152059c38397a023bd5daf65973
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-05-17 12:12:04 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,5 +1,11 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+   result = []
+   index = 0
+   for string in lista:
+        if string:
+            result.append(f"{index}. {string}")
+            index += 1
+   return result
 
 
 def enumerate_backwards(list):
