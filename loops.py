8c50dcdb4fb2085ec665cdfe584563d2dcc44f22
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-05-24 14:35:03 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,28 +1,37 @@
-def index_of_by_index(word, list, index):
-    return -1
-
-
-def index_of_empty(list):
-    return -1
-
-
-
-def index_of(word, list):
-    for i in range(len(list)):
-        if list[i] == word:
-            return i
-
-    return -1
-
-
-
-def put(word, list):
-    for i in range(len(list)):
-        if list[i] == "":
-            list[i] = word
+def index_of_by_index(color, colors, index):
+    for i in range(index, len(colors)):
+        if colors[i] == color:
             return i
     return -1
 
-
-def remove(word, list):
+def index_of_empty(lista):
+    color = ""
+    for index, i in enumerate(lista):
+        if color == lista[index]:
+            return index
     return -1
+
+def index_of(color, colors):
+    for index, i in enumerate(colors):
+        if color == colors[index]:
+            return index
+    return -1
+
+def put(word, lista):
+    for index, item in enumerate(lista):
+        if item == "":
+            lista[index] = word
+            return index
+    return -1
+
+
+def remove(word, lista):
+    i = 0
+    for index, item in enumerate(lista):
+        if item == word:
+            lista[index] = ""
+            i += 1
+    return i
+
+
+
